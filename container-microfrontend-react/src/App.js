24f62cc0748640bcadab3e6a64efbc6c55c6b8f7
Wallace Biscaro <wrbiscaro@hotmail.com>
2022-09-18 17:44:16 -0300

    Inclusao de prefixos para evitar conflitos de nomes de CSS em produção

--- container-microfrontend-react/src/App.js
+++ container-microfrontend-react/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+    //Adiciona um prefixo com iniciais do projeto nos nomes gerados pelo Material UI (css-in-js)
+    //Como cada projeto vai informar seu prefixo, não ocorrerá conflito de nomes de CSS devido a minificação de produção
+    productionPrefix: 'co'
+});
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
