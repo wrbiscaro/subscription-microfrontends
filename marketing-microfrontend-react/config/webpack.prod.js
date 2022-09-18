16ff3bda2f8f035adedc259e691b050c5b07ae45
Wallace Biscaro <wrbiscaro@hotmail.com>
2022-09-18 04:07:03 -0300

    Inclusao de public path no mfe de marketing

--- marketing-microfrontend-react/config/webpack.prod.js
+++ marketing-microfrontend-react/config/webpack.prod.js
@@ -6,7 +6,8 @@ const packageJson = require('../package.json');
 const prodConfig = {
     mode: 'production', //Com o modo production, o webpack faz algumas otimizações (minificacao, por exemplo)
     output: {
-        filename: '[name].[contenthash].js' //Todos os arquivos terão o seu nome + hash para evitar problemas de cache
+        filename: '[name].[contenthash].js', //Todos os arquivos terão o seu nome + hash para evitar problemas de cache
+        publicPath: '/marketing-microfrontend-react/latest/' //Diretorio do bucket s3 de onde o cloudffront vai carregar arquivos públicos (js, css, etc)
     },
     plugins: [
         new ModuleFederationPlugin({
